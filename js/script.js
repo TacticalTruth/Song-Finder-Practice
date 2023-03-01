1697bcb2969236b42fb62c3305c8dc9674a287bb
TacticalTruth <123516809+TacticalTruth@users.noreply.github.com>
2023-03-01 13:18:51 -0600

    working on tutorial

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let term = '';
+const songContainer = document.getElementById('songs')
+const updateTerm = () => {
+    term = document.getElementById('searchInput').value;
+    if (!term || term === '') {
+        alert('Please enter a search term.')
+    } else {
+
+        while(songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
+        }
+
+        const url = `https://itunes.apple.com/search?limit=12&media=music&term=${term}`;
+            fetch(url)
+            .then( (response) => response.json() )
+            .then((data) =>{
+                const artists = data.results;
+                return artists.map(result =>{
+                    const article = document.createElement('article'),
+                        artist = document.createElement('p'),
+                        song = document.createElement('p'),
+                        img = document.createElement('img'),
+                        audio = document.createElement('audio'),
+                        audioSource = document.createElement('source')
+
+                artist.innerHTML = result.artistName
+                song.innerHTML = result.trackName
+                img.src = result.artworkUrl100
+                audioSource.src = result.previewUrl
+                audio.setAttribute('controls', '')
+
+                article.appendChild(img)
+                article.appendChild(artist)
+                article.appendChild(song)
+                article.appendChild(audio)
+                audio.appendChild(audioSource)
+                songContainer.appendChild(article)
+                console.log(result)
+                })
+            });
+
+    }
+
+}
+
+    const searchBtn = document.querySelector('button')
+    searchBtn.addEventListener('click', updateTerm)
